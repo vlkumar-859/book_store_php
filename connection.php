168f2766c72c30239d38171b209525cb49db04fa
lohit valavala <61038912+vlkumar-859@users.noreply.github.com>
2021-07-11 18:05:49 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,9 +4,13 @@
 	$password = "";
 	$server = "127.0.0.1";
 	$db = "book_store";
-	
-	echo "bgv";
 
 	$conn = mysqli_connect($server,$username,$password,$db);
+	
+	if ($conn) {
+		echo "connected";
+	} else {
+		echo "not connected";
+	}
 
  ?>
